901390ae00550ac9dd75ccc67c2ef0a5ce1acf60
Matias Huartamendia <matias.huarta@gmail.com>
2022-07-01 02:34:52 -0300

    Small Fix Gallery #2
    Small Fix Gallery #2 - Now It Works

--- js/gallery.js
+++ js/gallery.js
@@ -51,17 +51,17 @@ function showSlides2(n) {
   let slides = document.getElementsByClassName("mySlides2");
   let dots = document.getElementsByClassName("demo2");
   let captionText = document.getElementById("caption2");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active";
+  captionText.innerHTML = dots[slideIndex2-1].alt;
 }
 
 
