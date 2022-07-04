dcf6dc42dab9ec58af762ea6f60a2de7367fa974
Matias Huartamendia <matias.huarta@gmail.com>
2022-07-03 23:59:23 -0300

    Gallery and Fixes
    Added a third gallery, fixes to menu, and font optimization

--- js/gallery.js
+++ js/gallery.js
@@ -64,5 +64,35 @@ function showSlides2(n) {
   captionText.innerHTML = dots[slideIndex2-1].alt;
 }
 
+/////////  Gallery 3 /////////
 
+let slideIndex3 = 1;
+showSlides3(slideIndex3);
 
+// Next/previous controls
+function plusSlides3(n) {
+  showSlides3(slideIndex3 += n);
+}
+
+// Thumbnail image controls
+function currentSlide3(n) {
+  showSlides3(slideIndex3 = n);
+}
+
+function showSlides3(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides3");
+  let dots = document.getElementsByClassName("demo3");
+  let captionText = document.getElementById("caption3");
+  if (n > slides.length) {slideIndex3 = 1}
+  if (n < 1) {slideIndex3 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex3-1].style.display = "block";
+  dots[slideIndex3-1].className += " active";
+  captionText.innerHTML = dots[slideIndex3-1].alt;
+}
